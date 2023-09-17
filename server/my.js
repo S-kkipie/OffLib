f68e22fb1b740d5503c54dda58830d8df9b6065e
S-kkipie <admamaniq@unsa.edu.pe>
2023-09-17 15:40:47 -0500

    eliminando la carpeta

--- server/my.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector(".arrow");
-const sidebar = document.querySelector(".sidebar");
-const main = document.querySelector("main");
-toggle.addEventListener("click", () =>{
-    sidebar.classList.toggle("close");
-    main.classList.toggle("close");
-
-})
